3dee4d16c474f208935c87415ba969a93f2ea126
Lafikateh <73808989+Lafikateh@users.noreply.github.com>
2021-07-04 22:29:43 +0200

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,22 +1,19 @@
 // C Standard Library Headers
 #include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// Program Headers
-#include "Main.h"
-#include "Files.h"
-#include "Strings.h"
-
 #define InsydeFlash_BIOSIMG_Signature_Length 16 
-const unsigned char InsydeFlash_BIOSIMG_Signature[] = { 0x24, 0x5F, 0x49, 0x46, 0x4C, 0x41, 0x53, 0x48, 0x5F, 0x42, 0x49, 0x4F, 0x53, 0x49, 0x4D, 0x47 }; // "$_IFLASH_BIOSIMG"
+const uint8_t InsydeFlash_BIOSIMG_Signature[16] = "$_IFLASH_BIOSIMG";
 
 typedef struct
 {
-	unsigned char signature[InsydeFlash_BIOSIMG_Signature_Length];
-	unsigned int full_size;
-	unsigned int used_size;
+	uint8_t signature[InsydeFlash_BIOSIMG_Signature_Length];
+	uint32_t full_size;
+	uint32_t used_size;
 } InsydeFlash_Firmware_Header;
 
 // Find an pattern in an buffer
@@ -55,14 +52,38 @@ unsigned char* find_pattern(const unsigned char* start, const unsigned char* end
 			slen -= bad_char_skip[start[last]];
 			start += bad_char_skip[start[last]];
 		}
-
-		// Return NULL
+		
 		return NULL;
 	}
 	else
 	{
-		// Return NULL
 		return NULL;
+	}
+}
+
+bool read_file(FILE* handle, void* buffer, unsigned int* size)
+{
+	if(handle != NULL && size != NULL)
+	{
+		if(buffer != NULL)
+		{
+			fseek(handle, 0, SEEK_END);
+			*size = ftell(handle);
+			fseek(handle, 0, SEEK_SET);
+			fread(buffer, 1, *size, handle);
+		}
+		else
+		{
+			fseek(handle, 0, SEEK_END);
+			*size = ftell(handle);
+			fseek(handle, 0, SEEK_SET);
+		}
+		
+		return true;
+	}
+	else
+	{
+		return false;
 	}
 }
 
@@ -70,7 +91,7 @@ unsigned char* find_pattern(const unsigned char* start, const unsigned char* end
 int main(int argument_count, char* argument_list[])
 {
 	// Print program header
-	printf("Insyde UEFI Firmware Extractor v1.0.1\n");
+	printf("Insyde UEFI Firmware Extractor v1.0.2\n");
 
 	if (argument_count == 1)
 	{
@@ -92,62 +113,57 @@ int main(int argument_count, char* argument_list[])
 		else
 		{
 			target_path = argument_list[1];
-			target_path = c_replace_string_c(target_path, ".FD", ".rom");
-			target_path = c_replace_string_c(target_path, ".fd", ".rom");
 		}
 
-		// Check if the source file exists
-		if (check_if_exists(source_path) == true)
+		// Attempt to open the source file
+		FILE* source_file = fopen(source_path, "rb");
+		if(source_file != NULL)
 		{
-			// Attempt to read the source file
-			data_buffer* source_buffer = read_file(source_path);
-			if (source_buffer != NULL)
+			// Get the source file size
+			unsigned int source_file_size = 0;
+			read_file(source_file, NULL, &source_file_size);
+			
+			// Read the source file
+			unsigned char* source_file_buffer = malloc(source_file_size);
+			bool read_result = read_file(source_file, source_file_buffer, &source_file_size);
+			if (read_result == true)
 			{
 				// Attempt to find the insydeflash firmware header
-				unsigned char* header_pointer = find_pattern(source_buffer->data, source_buffer->data + source_buffer->size, InsydeFlash_BIOSIMG_Signature, InsydeFlash_BIOSIMG_Signature_Length);
+				unsigned char* header_pointer = find_pattern(source_file_buffer, source_file_buffer + source_file_size, InsydeFlash_BIOSIMG_Signature, InsydeFlash_BIOSIMG_Signature_Length);
 				if (header_pointer != NULL)
 				{
 					// Map the header pointer to the source file buffer
 					InsydeFlash_Firmware_Header* header = (InsydeFlash_Firmware_Header*)(header_pointer);
 
-					// Allocate and zero-fill the target data buffer
-					unsigned long target_data_buffer_size = header->used_size;
-					unsigned char* target_data_buffer = calloc(target_data_buffer_size, sizeof(unsigned char));
+					// Allocate the target file buffer
+					unsigned long target_file_size = header->used_size;
+					unsigned char* target_file_buffer = malloc(target_file_size);
 
 					// Copy the UEFI Image to the target file buffer
 					unsigned char* firmware_image_pointer = header_pointer + sizeof(InsydeFlash_Firmware_Header);
-					memcpy(target_data_buffer, firmware_image_pointer, target_data_buffer_size);
+					memcpy(target_file_buffer, firmware_image_pointer, target_file_size);
 
-					// Create the target file buffer
-					data_buffer* target_buffer = create_buffer(target_data_buffer, target_data_buffer_size);
-
-					// Attempt to write the modified file
-					bool write_result = write_file(target_path, target_buffer);
+					// Write the modified file
+					FILE* destination_file = fopen(target_path, "wb");
+					fwrite(target_file_buffer, 1, target_file_size, destination_file);
 
 					// Free the file buffers
-					free_buffer(target_buffer);
-					free_buffer(source_buffer);
-
-					if (write_result == true)
-					{
-						return 0;
-					}
-					else
-					{
-						// Print error
-						printf("Failed to write file \"%s\"", target_path);
-
-						// Return failure
-						return 1;
-					}
+					free(target_file_buffer);
+					free(source_file_buffer);
+					
+					// Close the file handles
+					fclose(destination_file);
+					fclose(source_file);
+					
+					return 0;
 				}
 				else
 				{
 					// Print error
 					printf("InsydeFlash firmware signature not found in the file!\n");
-
-					// Free the file buffers
-					free_buffer(source_buffer);
+					
+					// Close the file handles
+					fclose(source_file);
 
 					// Return failure
 					return 1;
